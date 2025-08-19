b89ec8c9df084e05943d8e3cce25ac4fe4b554d9
Adriano <adrianogyn@gmail.com>
2025-08-19 12:27:24 -0300

    update

--- server/master.js
+++ server/master.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const SalaRoleta = require('./SalaRoleta');
 const SalaBaccarat = require('./SalaBaccarat');
 const SalaBacboo = require('./SalaBacboo');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
