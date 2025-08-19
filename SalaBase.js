4790cd22ed1df96b0adf364badc573792e23e0ea
Adriano <adrianogyn@gmail.com>
2025-08-19 00:16:36 -0300

    update

--- SalaBase.js
+++ SalaBase.js
@@ -1,92 +1,108 @@
-class SalaBase {
-  constructor(tipo, nome) 
+class SalaBase 
+{
+  constructor(tipo, nome)
   {
     this.tipo = tipo;
     this.nome = nome;
     this.clients = new Set();
     this.history = [];
+    this.tempoAposta = 10;
     this.status = 'aguardando'; // 'aberta', 'fechada', 'resultado', 'pagamento'
   }
-
-  adicionarCliente(ws) {
+  
+  adicionarCliente(ws)
+  {
     this.clients.add(ws);
     ws.send(JSON.stringify({ type: "history", data: this.history, tipo: this.tipo, room: this.nome }));
-    
     broadcastUserCount();
   }
-
-  removerCliente(ws) {
+  
+  removerCliente(ws)
+  {
     this.clients.delete(ws);
     broadcastUserCount();
   }
-
-  async iniciarRodada() {
-    while (true) {
-      await this.abrirApostas(2000);
+  
+  async iniciarRodada()
+  {
+    while (true)
+    {
+      await this.abrirApostas(this.tempoAposta * 1000);
       await this.fecharApostas(500);
       const resultado = await this.resultado();
       await this.pay(resultado, 500);
     }
   }
-
-  abrirApostas(ms) 
+  
+  abrirApostas(ms)
   {
     this.status = 'aberta';
     this.broadcastStatus();
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
-  fecharApostas(ms) {
+  
+  fecharApostas(ms)
+  {
     this.status = 'fechada';
     this.broadcastStatus();
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
+  
   // cada sala filha implementa seu próprio resultado
-  async resultado() 
+  async resultado()
   {
- //   throw new Error("Método resultado() precisa ser implementado na subclasse");
+    //   throw new Error("Método resultado() precisa ser implementado na subclasse");
   }
-
-  pay(resultado, ms) {
+  
+  pay(resultado, ms)
+  {
     this.status = 'pagamento';
     this.broadcastStatus();
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
-  broadcastStatus() 
+  
+  broadcastStatus()
   {
-    this.clients.forEach(client => 
+    this.clients.forEach(client =>
     {
-      if (client.readyState === WebSocket.OPEN) 
+      if (client.readyState === WebSocket.OPEN)
       {
         client.send(JSON.stringify({ type: "status", data: this.status, tipo: this.tipo, room: this.nome }));
       }
     });
   }
-
-  broadcastNewCard(carta, destino) 
+  
+  broadcastNewCard(carta, destino)
   {
     this.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN)
+      {
         client.send(JSON.stringify({ type: "newCard", data: { carta, destino }, tipo: this.tipo, room: this.nome }));
       }
     });
   }
-
-  broadcastNewData(data) {
-    this.clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
+  
+  broadcastNewData(data) 
+  {
+    this.clients.forEach(client =>
+    {
+      if (client.readyState === WebSocket.OPEN)
+      {
         client.send(JSON.stringify({ type: "newData", data, tipo: this.tipo, room: this.nome }));
       }
     });
   }
   
-  broadcastUserCount() {
-  const msg = JSON.stringify({ type: "userCount", data: { room: this.nome, count: this.clients.size } });
-  this.clients.forEach(c => {
-    if (c.readyState === c.OPEN) c.send(msg);
-  });}
+  broadcastUserCount()
+  {
+    const msg = JSON.stringify({ type: "userCount", data: { room: this.nome, count: this.clients.size } });
+    this.clients.forEach(c => 
+    {
+        if (c.readyState === c.OPEN) 
+            c.send(msg);
+    });
+    
+  }
 }
 
 module.exports = SalaBase;
