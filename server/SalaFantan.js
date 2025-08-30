742f63e2dbe63dc5ef9873a925f9676c12cefbf2
Adriano <add>
2025-08-30 02:42:04 -0300

    update SalaFantan.js

--- server/SalaFantan.js
+++ server/SalaFantan.js
@@ -7,8 +7,8 @@ class SalaFantan extends SalaBase {
     
     async GerarResultado() {
         let result = {
-            numero = 0,
-            total = 50,
+            numero : 0,
+            total : 50,
             vencedor: "*",
             time: new Date().toISOString()
         };
